a9c4aec9f34a05d671c747a63244161f8de0b332
Emanoel Rainey <eraineycm@hotmail.com>
2023-12-18 21:34:11 -0300

    Atualização das dimensões

--- schema/dimensional_schema.sql
+++ schema/dimensional_schema.sql
@@ -1,4 +1,31 @@
-create table dim_tempo (
-id serial primary key,
-ano int
+CREATE SCHEMA profile_prouni_beneficiaries;
+
+USE profile_prouni_beneficiaries;
+
+CREATE TABLE dim_tempo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ano INT
+);
+
+CREATE TABLE dim_local (
+    id_local INT AUTO_INCREMENT PRIMARY KEY,
+    regiao VARCHAR(100),
+    estado VARCHAR(100),
+    cidade VARCHAR(100)
+);
+
+CREATE TABLE dim_perfil_bolsa (
+    id_perfil_bolsa INT AUTO_INCREMENT PRIMARY KEY,
+    tp_bolsa VARCHAR(100),
+    modalidade VARCHAR(100),
+    turno VARCHAR(100)
+);
+
+
+CREATE TABLE dim_perfil_beneficiario (
+    id_perfil_beneficiario INT AUTO_INCREMENT PRIMARY KEY,
+    faixa_etaria VARCHAR(50),
+    sexo VARCHAR(50),
+    condicao_fisica VARCHAR(100),
+    grupo_racial VARCHAR(50)
 );
